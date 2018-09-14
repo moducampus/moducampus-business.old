59e8a5338325e73edf3611fac3e456f467cd686c
drake-jin <dydwls121200@gmail.com>
2018-09-14 16:01:00 +0900

    4:00

---
+++ README.md
@@ -0,0 +1,19 @@
+# 모두의 캠퍼스 비지니스 포트폴리오 페이지
+
+# 실행 
+
+``` bash
+$ yarn install
+$ yarn start
+```
+
+# 사용라이브러리
+
+#### 클라이언트
+
+- bootstrap.css
+- jquery 3.x
+
+#### 서버
+
+- express
---
+++ src/app.js
@@ -0,0 +1,26 @@
+// 외장모듈
+const express = require('express')
+
+const app = express()
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static('assets'));
+app.use('/', (req, res) => {
+  res.render('index.html');
+});
+
+app.listen(3001, () => {
+  console.log("Express server has started on port 3001")
+});
+
+/*
+
+CYJ
+TODO:
+- [] 정적인 파일 로드할 수 있도록하기 url: /assets/*
+- [] ejs코드가 돌아가도록 서버사이드 렌더링을 시작하기
+
+*/
